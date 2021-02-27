30507fb5b50b6d80cd9034015e18347fd70979e2
tuimac <tuimac.devadm01@gmail.com>
2021-02-27 07:51:00 +0000

    customize for this project

--- docker/dev/postman.sh
+++ docker/dev/postman.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-[[ -z $1 ]] && { echo 'There is no argument.'; exit 1; }
+URL='http://localhost:8000/runcommand'
 
-curl -X POST -H "Content-Type: application/json" \
+[[ -z $URL ]] && { echo 'There is no argument.'; exit 1; }
+
+RESULT=$(curl -X POST -H "Content-Type: application/json" \
     -d '{"Name": "test", "command": "docker ps -a"}' \
-    $1 | jq
+    $URL)
+
+echo $RESULT | jq -r
