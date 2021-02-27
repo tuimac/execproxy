fccf1913f5dcb00e7139e38a2531c77e0b7a5baa
tuimac <tuimac.devadm01@gmail.com>
2021-02-27 15:00:08 +0000

    fixed

---
+++ src/frontend/src/app/runcommand/runcommand.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-runcommand',
+  templateUrl: './runcommand.component.html',
+  styleUrls: ['./runcommand.component.scss']
+})
+export class RuncommandComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
