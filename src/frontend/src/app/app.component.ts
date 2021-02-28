f69ef2ce8fb59a14e315201e09ffc6ed77b02898
tuimac <tuimac.devadm01@gmail.com>
2021-02-28 08:25:00 +0000

    fixed detail

--- src/frontend/src/app/app.component.ts
+++ src/frontend/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'root',
+  selector: 'execproxy-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
