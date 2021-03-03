77c3a85404ecbf0420bfe53830ea5e17c2c9dca9
tuimac <tuimac.devadm01@gmail.com>
2021-03-03 12:45:31 +0000

    fixed

--- src/frontend/src/app/app-routing.module.ts
+++ src/frontend/src/app/app-routing.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RuncommandComponent } from './runcommand/runcommand.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'dashboard',
+    redirectTo: '/dashboard',
     pathMatch: 'full',
   },
   {
     path: 'runcommand',
     component: RuncommandComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
   }
 ];
 
